a4c57ba75d88b4ba02ec554888ba2b11c0d53b73
Nitipat <nitipat@Nitipats-MacBook-Pro.local>
2016-06-10 13:44:00 +0700

    ok

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::post('/editprofile', [
+	'middleware' => 'auth',
+    'uses' => 'ProfileController@editprofile']
+);
