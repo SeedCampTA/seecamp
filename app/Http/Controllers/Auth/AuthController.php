248f6892f8027142e229c7f4a651c5448f3157f6
kamonkwun-mbp <kamonkwun@thinknet.co.th>
2016-06-10 13:44:21 +0700

    test

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
             'password' => 'required|min:6|confirmed',
             'firstname' => 'required|max:255',
             'lastname' => 'required|max:255',
-            'image' => 'required|image:jpg,jpeg,png,gif,bmp|size:4000',
+            // 'image' => 'required|image:jpg,jpeg,png,gif,bmp|size:4000',
         ]);
     }
 
@@ -71,7 +71,7 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
             'firstname' => $data['firstname'],
             'lastname' => $data['lastname'],
-            'image' => $data['image'],
+            // 'image' => $data['image'],
         ]);
     }
 }
