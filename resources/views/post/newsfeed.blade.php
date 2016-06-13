beec19123d2b3d1f6d692e977cda63b05a23bec4
Nutchalum <nutchalum@thinknet.co.th>
2016-06-13 09:30:07 +0700

    re arrange

--- resources/views/post/newsfeed.blade.php
+++ resources/views/post/newsfeed.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('js')
-    <script src="{{ asset('js/comment-post.js') }}"></script>
+<script src="{{ asset('js/comment-post.js') }}"></script>
 @endsection
 
 @section('content')
@@ -16,7 +16,9 @@
             <div class="form-group" style="padding:14px;">
                 <textarea class="form-control" placeholder="Update your status"></textarea>
             </div>
+
             <button class="btn btn-primary pull-right" type="button">Post</button>
+
             <ul class="list-inline">
                 <a href="">
                     <i class="glyphicon glyphicon-camera"></i>
@@ -24,6 +26,7 @@
             </ul>
         </form>
     </div>
+
     @foreach ($posts as $post)
     <div class="panel panel-info">
         <div class="panel-heading panel-feed">
@@ -32,10 +35,12 @@
                   <img src="http://www.bootply.com/assets/example/bg_5.jpg" alt="" class="img-circle">
                 </a>
             </div>
+
             <div class="col-xs-6 feed-username">
             {{ $post->user->username }}
             </div>
         </div>
+
         <div class="panel-body">
             <div class="clearfix"></div>
             <p>{{ $post->msg }}</p>
@@ -47,25 +52,24 @@
             </div>
             <hr>
             <p>45 Likes</p>
-                <div class="input-group">
-                    <div class="input-group-btn">
-                        @if ($post->likeable)
-                            <button class="btn btn-default" onclick="likePost()">
-                                +1
-                            </button>
-                        @else
-                            <button class="btn btn-default" onclick="unlikePost()">
-                                -1
-                            </button>
-                        @endif
-                        <button class="btn btn-default" onclick="commentPost({{ $post->id }})">
-                            <i class="glyphicon glyphicon-comment"></i>
+            <div class="input-group">
+                <div class="input-group-btn">
+                    @if ($post->likeable)
+                        <button class="btn btn-default" onclick="likePost()">
+                            +1
                         </button>
-                    </div>
-                    {{ csrf_field() }}
-                    <input id="post-comment-{{ $post->id }}" type="text" class="form-control" placeholder="Add a comment..">
+                    @else
+                        <button class="btn btn-default" onclick="unlikePost()">
+                            -1
+                        </button>
+                    @endif
+                    <button class="btn btn-default" onclick="commentPost({{ $post->id }})">
+                        <i class="glyphicon glyphicon-comment"></i>
+                    </button>
                 </div>
-                </form>
+
+                <input id="comment-message-{{ $post->id }}" type="text" class="form-control" placeholder="Add a comment..">
+            </div>
             <div class="divider"></div>
             <ul class="list-group">
                 @foreach ($post->comments as $comment)
