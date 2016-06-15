400c99dc52b3a6c5ec77a1f34fd4c9afeb20f4e5
Wudtichai Chumsangchotsagun <wudtichai@thinknet.co.th>
2016-06-15 18:02:23 +0700

    ฝากด้วย

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,26 +19,37 @@
     @yield('css')
 </head>
 <body id="app-layout">
-  <nav id="seedcamp-nav" class="navbar navbar-default navbar-static-top">
-  <a id="seedcamp-header" class="navbar-brand" href="{{ url('/') }}">Social Network by THiNKNET</a>
-  @if (Auth::guest())
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="{{ url('/login') }}">Login</a></li>
-      <li><a href="{{ url('/register') }}">Register</a></li>
-    </ul>
-  @else
-    <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-            {{ Auth::user()->username }} <span class="caret"></span>
-        </a>
-        <ul class="dropdown-menu" role="menu">
-            <li><a href="{{ url('/profile/edit') }}"><i class="fa fa-btn"></i>Profile</a></li>
-            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+  <nav id="seedcamp-nav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a id="seedcamp-header" class="navbar-brand" href="{{ url('/') }}">Social Network by THiNKNET</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+        @if (Auth::guest())
+          <li><a href="{{ url('/login') }}">Login</a></li>
+          <li><a href="{{ url('/register') }}">Register</a></li>
+        @else
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->username }}</a>
+            <ul class="dropdown-menu">
+              <li><a href="{{ url('/profile/edit') }}"><i class="fa fa-btn"></i>Profile</a></li>
+              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+            </ul>
+          </li>
+        @endif
         </ul>
-      </li>
-    </ul>
-  @endif
+      </div><!-- /.navbar-collapse -->
+    </div>
   </nav>
   @yield('content')
   {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
