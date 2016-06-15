5a489eedb2a663d4927a1969df027b0512d0809c
Nutchalum <nutchalum@thinknet.co.th>
2016-06-15 14:40:29 +0700

    fix post

--- public/js/comment-post.js
+++ public/js/comment-post.js
@@ -1,12 +1,11 @@
 function commentPost(id)
 {
-    comment_message = $('#post-comment-' + id).val();
+    comment_message = $('#comment-message-' + id).val();
     csrf = $('meta[name="csrf-param"]').attr('content');
-
     $.ajax({
         url: '/posts/' + id + '/comments/',
         headers: {
-        	'X-CSRF-TOKEN': csrf,
+            'X-CSRF-TOKEN': csrf,
         },
         data: {
             'comment': comment_message,
