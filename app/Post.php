df41178787386f3dbe3f402156b254fe00a7b41c
Wudtichai Chumsangchotsagun <wudtichai@thinknet.co.th>
2016-06-15 16:03:10 +0700

    img -> image

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-	protected $guarded = ['id', 'user_id', 'img'];
+	protected $guarded = ['id', 'user_id', 'image'];
 
 	protected $with = ['user', 'likeByUsers'];
 
