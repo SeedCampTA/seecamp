51f55fe17edee2989ca72171dc1811d591e18fac
Nutchalum <nutchalum@thinknet.co.th>
2016-06-15 16:24:42 +0700

    comment in newsfeed

--- public/js/comment-post.js
+++ public/js/comment-post.js
@@ -2,14 +2,19 @@ function commentPost(id)
 {
     comment_message = $('#comment-message-' + id).val();
     csrf = $('meta[name="csrf-param"]').attr('content');
-    $.ajax({
-        url: '/posts/' + id + '/comments/',
-        headers: {
-            'X-CSRF-TOKEN': csrf,
-        },
-        data: {
-            'comment': comment_message,
-        },
-        method: 'POST',
-    });
+    if (comment_message != '') {
+        $.ajax({
+            url: '/posts/' + id + '/comments/',
+            headers: {
+                'X-CSRF-TOKEN': csrf,
+            },
+            data: {
+                'comment': comment_message,
+            },
+            method: 'POST',
+            success: function(comment) {
+                window.alert('comment success');
+            }
+        });
+    }
 };
