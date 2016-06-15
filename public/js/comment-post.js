45188f1ca15e1a9d3523350d90fc42ae7f2c9a11
Nattawut <nattawut@thinknet.co.th>
2016-06-15 18:50:21 +0700

    fix like

--- public/js/comment-post.js
+++ public/js/comment-post.js
@@ -59,11 +59,10 @@ function unlikePost(post_id)
         },
         method: 'PUT',
         success: function(jsonData) {
-            getLike(post_id);
-            $('#post_' + post_id + ' .btn-like').attr('onClick', 'likePost(' + post_id + ')');
             $('#post_' + post_id + ' .btn-like').text('+1');
             $('#post_' + post_id + ' .btn-like').toggleClass('btn-seedcamp');
-            
+            getLike(post_id);
+            $('#post_' + post_id + ' .btn-like').attr('onClick', 'likePost(' + post_id + ')');
         },
         error: function(jsonData) {
             alert(jsonData);
@@ -82,11 +81,10 @@ function likePost(post_id)
         },
         method: 'PUT',
         success: function(jsonData) {
-            getLike(post_id);
-            $('#post_' + post_id + ' .btn-like').attr('onClick', 'unlikePost(' + post_id + ')');
             $('#post_' + post_id + ' .btn-like').text('-1');
             $('#post_' + post_id + ' .btn-like').toggleClass('btn-seedcamp');
-            
+            getLike(post_id);
+            $('#post_' + post_id + ' .btn-like').attr('onClick', 'unlikePost(' + post_id + ')');
         },
         error: function(jsonData) {
             alert(jsonData);
