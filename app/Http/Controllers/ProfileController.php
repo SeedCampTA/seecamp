24c29f9e2c7dc33c861b202ab55b4c47a6b8ec7d
lusiaskolonie <lusiaskolonie@gmail.com>
2016-07-03 15:51:59 +0700

    clean

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,4 +12,16 @@ class ProfileController extends Controller
         return view('user.profile')->with('user', \Auth::user());
     }
 
+    public function update(\App\Http\Requests\StoreProfileRequest $request)
+    {
+        $data = $request->all();
+        $user = \Auth::user();
+        $user->update($data);
+
+        $updateMsg = 'Update profile completed';
+
+        return redirect('/profile/edit')
+            ->with('updateMsg', $updateMsg);
+    }
+
 }
