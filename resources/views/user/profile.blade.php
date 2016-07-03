7f1a122ac5708308caf883c45d18c2c7543decf4
lusiaskolonie <lusiaskolonie@gmail.com>
2016-07-03 15:39:28 +0700

    clean

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -9,8 +9,6 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
 
-
-
       <div class="panel panel-default">
         <div class="panel-heading">Edit Profile</div>
         <div class="panel-body">
@@ -19,33 +17,48 @@
             <input type="hidden" name="_method" value="PUT">
             <div class="row">
               <div class="col-xs-12 profile-image-panel">
-                <img src="{{ $user->image }}" alt="profile picture" class="profile-image">
+                <img src="" alt="profile picture" class="profile-image">
               </div>
             </div>
-            <div class="form-group">
+            <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
               <label for="firstname" class="col-md-4 control-label">First Name</label>
 
               <div class="col-md-6">
                 <input id="firstname" type="firstname" class="form-control" name="firstname" value="">
 
+                @if ($errors->has('firstname'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('firstname') }}</strong>
+                  </span>
+                @endif
               </div>
             </div>
 
-            <div class="form-group">
+            <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
               <label for="lastname" class="col-md-4 control-label">Last Name</label>
 
               <div class="col-md-6">
                 <input id="lastname" type="lastname" class="form-control" name="lastname" value="">
 
+                @if ($errors->has('lastname'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('lastname') }}</strong>
+                  </span>
+                @endif
               </div>
             </div>
 
-            <div class="form-group">
+            <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
               <label for="image" class="col-md-4 control-label">Profile Image</label>
 
               <div class="col-md-6">
                 <input id="image" type="file" class="form-control" name="image">
 
+                @if ($errors->has('image'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('image') }}</strong>
+                  </span>
+                @endif
               </div>
             </div>
 
